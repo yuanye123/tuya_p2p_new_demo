8dd2e35e3ab898f846d5f52b12a69dbb143c5a4d
xushengju <shengju0517@163.com>
2021-09-27 16:58:16 +0800

    add project review by huangliuchuang

---
+++ README.md
@@ -0,0 +1,175 @@
+## P2P SDK说明
+p2p sdk集成了iot功能与p2p功能
+
+## P2pJniApi 说明
+### 初始化sdk
+```java
+ /**
+     * sdk初始化
+     * @param pid 产品id
+     * @param uid  uid
+     * @param key key
+     * @param path  存储路径,类似"/sdcard/"
+     * @param version  版本名
+     * @param netcardName 网卡名
+     * @return 0 成功
+     *         非0 失败
+     */
+    external fun initSdk(
+        pid: String, uid: String, key: String, path: String,
+        version: String, netcardName: String
+    ): Int
+```
+### 初始化p2p
+```java
+    /**
+     * 初始化p2p
+     * @return 0 成功
+     *         非0 失败
+     */
+    external fun initTransP2p(): Int
+  ```
+### 关闭p2p连接
+```java
+    /**
+     * 关闭p2p连接
+     */
+    external fun closeTransP2p()
+```
+### 多个dp上传接口
+```java
+ 	/**
+     * 多个dp上传接口
+     * @param hasTime 是否使用时间戳
+     * @param events  上传dp数组
+     * @return 0 成功
+     *        非0 失败
+     */
+    external fun do_report(hasTime: Boolean, events: Array<DPEvent>): Int
+```
+### 上传单个dp
+```java
+	/**
+     * 上传单个dp
+     * @param dpi dip
+     * @param type dp类型
+     * @param value dp数据
+     * @param time 时间戳
+     * @return 0 成功
+     *         非0 失败
+     */
+    external fun do_report(dpid: Int, type: Int, value: Any?, time: Int): Int
+```
+### 监听的mq协议类型
+```java
+	 /**
+     * 监听的mq协议类型
+     * @param protocol  协议类型
+     * @return 0 成功
+     *        非0 失败
+     */
+    external fun regist_mqtt_msg(protocol: Int): Int
+```
+### 上传mq message
+```java
+/**
+     * 上传mq message
+     * @param protocol 协议类型
+     * @param msg 消息
+     * @return 0 成功
+     *        非0 失败
+     */
+    external fun send_mqtt_msg(protocol: Int, msg: String): Int
+```
+### Http请求
+```java
+	/**
+     * Http请求
+     * @param apiName  
+     * @param  apiVersion
+     * @param jsonMsg 
+     */
+    external fun http_request(apiName: String, apiVersion: String, jsonMsg: String): HttpResponse
+```
+### 获取 deviceId
+```java
+	/**
+     * get deviceId
+     * @return
+     */
+    external fun getDeviceId(): String
+```
+###重置设备
+```java
+ 	/**
+     * unreset device
+     * @return ret
+     */
+    external fun unActive(): Int
+```
+### p2p sdk回调
+```java
+interface P2pCallBack {
+        /**
+         * 云端解绑设备回调
+         */
+        fun onReset()
+
+        //
+        /**
+         * 收到配网二维码短链
+         * @param url 二维码连接
+         */
+        fun onShorturl(url: String?)
+
+        //接收到的图视频数据
+        fun recvVideData(
+            sessionId: Int,
+            status: RecvStatus,
+            buf: ByteArray,
+            len: Int,
+            downloadAlbumHead: Download_Album_Head,
+            c2cCmdIoCtrlAlbumDownloadStart: C2C_CMD_IO_CTRL_ALBUM_DOWNLOAD_START
+        )
+
+        /**
+         * mq网络状态变化
+         * @param 
+         */
+        fun onNetStauts(status: Int)
+
+        /**
+         * mqtt消息回调
+         * @param protocol 协议号
+         * @param msgObj 消息
+         */
+        fun onMqttMsg(protocol: Int, msgObj: JSONObject?)
+
+        /**
+         * dp事件接收
+         *
+         * @param event 事件类型
+         * DPEvent.Type.PROP_BOOL
+         * DPEvent.Type.PROP_VALUE
+         * DPEvent.Type.PROP_STR
+         * DPEvent.Type.PROP_ENUM
+         * DPEvent.Type.PROP_BITMAP
+         * DPEvent.Type.PROP_RAW
+         */
+        fun onDpEvent(event: DPEvent)
+    }
+```
+### 发送多个dp事件
+```java
+	/**
+     * 发送多个dp事件
+     *
+     * @param events 多个dp类型
+     * @return 0 成功
+     *         非0 失败 
+     */
+    fun sendDP(events: List<DPEvent>): Int
+```
+
+
+
---
+++ app/src/main/java/com/tuya/p2p_android/MainActivity.java
@@ -0,0 +1,134 @@
+package com.tuya.p2p_android;
+
+import android.Manifest;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tuya.p2p.C2C_CMD_IO_CTRL_ALBUM_DOWNLOAD_START;
+import com.tuya.p2p.DPEvent;
+import com.tuya.p2p.Download_Album_Head;
+import com.tuya.p2p.Log;
+import com.tuya.p2p.P2pJniApi;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
+
+import org.json.JSONObject;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG = MainActivity.class.getSimpleName();
+
+    private String[] requiredPermissions = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    private final int PERMISSION_CODE = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.init(this, "/sdcard/p2p", 2);
+
+//        pid: String, uid: String, key: String, path: String,
+//                version: String, netcardName: String
+
+        if (!EasyPermissions.hasPermissions(this, requiredPermissions)) {
+            EasyPermissions.requestPermissions(this, "需要授予权限以使用设备", PERMISSION_CODE, requiredPermissions);
+        } else {
+            initSDK();
+        }
+
+//        int dpid, byte type, Object value, int time_stamp
+//        DPEvent dpEvent = new DPEvent(1, "1".getBytes()[0], new Object(), -1);
+//
+//        ArrayList<DPEvent> dpEvents = new ArrayList<>();
+//        dpEvents.add(dpEvent);
+//        P2pJniApi.INSTANCE.sendDP(dpEvents);
+        findViewById(R.id.txt_reset).setOnClickListener(v -> {
+            P2pJniApi.INSTANCE.unActive();
+        });
+    }
+
+    private void initSDK() {
+        P2pJniApi.INSTANCE.setP2pCallBack(new P2pJniApi.P2pCallBack() {
+            @Override
+            public void recvVideData(int sessionId, P2pJniApi.RecvStatus status, byte[] buf, int len, Download_Album_Head downloadAlbumHead, C2C_CMD_IO_CTRL_ALBUM_DOWNLOAD_START c2cCmdIoCtrlAlbumDownloadStart) {
+                android.util.Log.d(TAG, "channel is " + c2cCmdIoCtrlAlbumDownloadStart.filename + Thread.currentThread().getName() + "recvVideData status is " + status + "  download is packsize is " + downloadAlbumHead.packageSize + " fileName is " + downloadAlbumHead.fileName);
+            }
+
+            @Override
+            public void onDpEvent(DPEvent event) {
+
+            }
+
+            @Override
+            public void onMqttMsg(int protocol, JSONObject msgObj) {
+
+            }
+
+            @Override
+            public void onNetStauts(int status) {
+                android.util.Log.d(TAG, "onNetStauts status is " + status);
+                if (status == 7) {
+                    P2pJniApi.INSTANCE.initTransP2p();
+                    android.util.Log.d(TAG, "get device id is " + P2pJniApi.INSTANCE.getDeviceId());
+                }
+            }
+
+            @Override
+            public void onReset() {
+                android.util.Log.d(TAG, "onReset ++++");
+//                P2pJniApi.INSTANCE.closeTransP2p();
+                Intent mStartActivity = getPackageManager().getLaunchIntentForPackage(getPackageName());
+                if (mStartActivity != null) {
+                    int mPendingIntentId = 123456;
+                    PendingIntent mPendingIntent = PendingIntent.getActivity(MainActivity.this, mPendingIntentId
+                            , mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+                    AlarmManager mgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 2000, mPendingIntent);
+                    Runtime.getRuntime().exit(0);
+                }
+            }
+
+            @Override
+            public void onShorturl(String url) {
+                android.util.Log.d(TAG, "onShorturl is " + url);
+                if(url != null){
+                    final String localUrl = (String) com.alibaba.fastjson.JSONObject.parseObject(url).get("shortUrl");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ImageView qrcode = findViewById(R.id.qrcode);
+                            qrcode.setVisibility(View.VISIBLE);
+                            qrcode.setImageBitmap(CodeUtils.createImage(localUrl, 400, 400, null));
+                        }
+                    });
+                }
+            }
+        });
+        P2pJniApi.INSTANCE.initSdk(BuildConfig.PID, BuildConfig.UUID, BuildConfig.AUTHOR_KEY, "/sdcard/", "1.0.0", "wlan0");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PERMISSION_CODE) {
+            if (!EasyPermissions.hasPermissions(this, requiredPermissions)) {
+                EasyPermissions.requestPermissions(this, "需要授予权限以使用设备", PERMISSION_CODE, requiredPermissions);
+            } else {
+                initSDK();
+            }
+        }
+    }
+}
